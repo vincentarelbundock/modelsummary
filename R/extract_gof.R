55bf48db2dc308afae8a106461436e0e0d6743b3
Vincent Arel-Bundock <varel@umich.edu>
2019-01-20 14:55:28 -0500

    break up functions into separate files

---
+++ R/extract_gof.R
@@ -0,0 +1,35 @@
+#' Extract goodness-of-fit statistics from a single model
+#'
+extract_gof <- function(model, fmt = '%.3f', gof_map = NULL, ...) {
+    if (is.null(gof_map)) {
+        gof_map <- gtsummary::gof_map
+    }
+    # sanity checks
+    sanity_check(gof_map = gof_map)
+    # extract gof from model object
+    gof <- broom::glance(model) 
+    # round numeric values and rename 
+    for (column in colnames(gof)) {
+        # is gof in gof_map?
+        idx <- match(column, gof_map$raw)
+        if (!is.na(idx)) { # yes
+            if (class(gof[[column]]) %in% c('numeric', 'integer')) {
+                gof[[column]] <- rounding(gof[[column]], gof_map$fmt[idx]) 
+            } else {
+                gof[[column]] <- as.character(gof[[column]])
+            }
+            colnames(gof)[colnames(gof) == column] <- gof_map$clean[idx] 
+        } else { # no
+            if (class(gof[[column]]) %in% c('numeric', 'integer')) {
+                gof[[column]] <- rounding(gof[[column]], fmt)
+            } else {
+                gof[[column]] <- as.character(gof[[column]])
+            }
+        }
+    }
+    # reshape
+    gof <- gof %>% 
+           tidyr::gather(term, value)
+    # output
+    return(gof)
+}
---
+++ R/gof_map.R
@@ -0,0 +1,6 @@
+#' Data.frame used to clean up and format goodness-of-fit statistics
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gof_map
+#' @format data.frame with 3 columns of character data: raw, clean, fmt
