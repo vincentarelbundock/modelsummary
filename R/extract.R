36b5f9390f091519fe8a7b12620d239fc447bfce
Vincent Arel-Bundock <varel@umich.edu>
2019-01-23 18:43:07 -0500

    inherit parameters to avoid doc duplication

--- R/extract.R
+++ R/extract.R
@@ -1,25 +1,7 @@
 #' Extract and combine estimates and goodness-of-fit statistics from several
 #' statistical models.
 #'
-#' @param models named list of models to summarize
-#' @param statistic string name of the statistic to include in parentheses
-#' below estimates. Must be either "conf.int", or one of the column names
-#' produced by the `broom::tidy` function. Typical values include: "std.error",
-#' "conf.int", "statistic", "p.value".
-#' @param conf_level confidence level to use for confidence intervals
-#' @param coef_omit string regular expression. Omits all matching coefficients
-#' from the table (using `stringr::str_detect`).
-#' @param coef_map named string vector. Names refer to the original variable
-#' names. Values refer to the variable names that will appear in the table.
-#' Coefficients which are omitted from this vector will be omitted from the
-#' table. The table will be ordered in the same order as this vector.
-#' @param gof string regular expression. Omits all matching gof statistics from
-#' the table (using `stringr::str_detect`).
-#' @param gof_map data.frame in the same format as `gtsummary::gof_map`
-#' @param add_rows list of character vectors, each of length equal to the number
-#' of models + 1.
-#' @param fmt passed to the `sprintf` function to format numeric values into
-#' strings, with rounding, etc. See `?sprintf` for options.
+#' @inheritParams gtsummary
 #'
 #' @export
 extract <- function(models,
