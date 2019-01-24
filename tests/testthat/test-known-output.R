320857a34c0eed37dcd921fec68d502d06a6c1d0
Vincent Arel-Bundock <varel@umich.edu>
2019-01-23 20:09:25 -0500

    more known-value tests

--- tests/testthat/test-known-output.R
+++ tests/testthat/test-known-output.R
@@ -5,7 +5,7 @@ library(MASS)
 library(gtsummary)
 library(tidyverse)
 url <- 'https://vincentarelbundock.github.io/Rdatasets/csv/HistData/Guerry.csv'
-dat <- read.csv(url) 
+dat <- read.csv(url)
 dat$Clergy <- ifelse(dat$Clergy > 40, 1, 0) # binary variable for logit model
 models <- list()
 models[['OLS 1']] <- lm(Literacy ~ Crime_prop + Infants, dat)
@@ -14,7 +14,7 @@ models[['OLS 2']] <- lm(Desertion ~ Crime_prop + Infants, dat)
 models[['NBin 2']] <- glm.nb(Desertion ~ Crime_prop + Donations, dat)
 models[['Logit 1']] <- glm(Clergy ~ Crime_prop + Infants, dat, family = binomial())
 
-test_that("html output", {
+test_that("html_output: complex table", {
 	cm <- c('Crime_prop' = 'Crime / Population',
 		'Donations' = 'Donations',
 		'Infants' = 'Infants',
@@ -32,4 +32,57 @@ test_that("html output", {
            gt::tab_spanner(label = 'Clergy', columns = 'Logit 1') %>%
            gt::as_raw_html()
     expect_known_output(cat(raw), "known_output/complex_table.html")
+})
+
+test_that("html_output: uncertainty estimates", {
+    raw <- gtsummary(models, statistic = 'std.error') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/std.error.html")
+
+    raw <- gtsummary(models, statistic = 'p.value') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/p.value.html")
+
+    raw <- gtsummary(models, statistic = 'statistic') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/statistic.html")
+
+    raw <- gtsummary(models, statistic = 'conf.int', conf_level = .99) %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/conf.int.html")
+})
+
+test_that("html_output: title and subtitle", {
+    raw <- gtsummary(models,
+                     title = 'This is a title for my table.') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/title.html")
+    raw <- gtsummary(models,
+                     subtitle = 'And this is the subtitle.') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/subtitle.html")
+    raw <- gtsummary(models,
+                     title = 'This is a title for my table.',
+                     subtitle = 'And this is the subtitle.') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/title_subtitle.html")
+})
+
+test_that("html_output: rounding", {
+    raw <- gtsummary(models, fmt = '%.7f') %>%
+           gt::as_raw_html()
+    expect_known_output(cat(raw), "known_output/rounding.html")
+})
+
+test_that("html_output: background color", {
+    raw <- gtsummary(models) %>%
+           tab_style(style = cells_styles(bkgd_color = "lightcyan",
+                                          text_weight = "bold"),
+                     locations = cells_data(columns = vars(`OLS 1`))) %>%
+           tab_style(style = cells_styles(bkgd_color = "#F9E3D6",
+                                          text_style = "italic"),
+                     locations = cells_data(columns = vars(`NBin 2`),
+                                            rows = 2:6)) %>%
+           as_raw_html()
+    expect_known_output(cat(raw), "known_output/background_color.html")
 })
