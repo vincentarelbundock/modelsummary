d4fbec50f0770dc460e367528e31f4673809b1e7
Vincent Arel-Bundock <varel@umich.edu>
2019-01-24 12:36:37 -0500

    stars_note argument

--- R/extract_estimates.R
+++ R/extract_estimates.R
@@ -1,22 +1,24 @@
 #' Extract estimates and statistics from a single model
-extract_estimates <- function(model, 
-                             statistic = 'std.error', 
+extract_estimates <- function(model,
+                             statistic = 'std.error',
                              conf_level = .95,
-                             fmt = '%.3f', 
-                             stars = NULL, 
+                             fmt = '%.3f',
+                             stars = NULL,
                              ...) {
+
     # extract estimates with confidence intervals
     if (statistic == 'conf.int') {
-        est <- broom::tidy(model, conf.int = TRUE, conf.level = conf_level) 
+        est <- broom::tidy(model, conf.int = TRUE, conf.level = conf_level)
         if (!all(c('conf.low', 'conf.high') %in% colnames(est))) {
             stop('broom::tidy cannot not extract confidence intervals for a model of this class.')
-        } 
+        }
         est <- est %>%
               # rounding
               dplyr::mutate(estimate = rounding(estimate, fmt),
                             conf.low = rounding(conf.low, fmt),
                             conf.high = rounding(conf.high, fmt),
                             statistic = paste0('[', conf.low, ', ', conf.high, ']'))
+
     # extract estimates with another statistic
     } else {
         est <- broom::tidy(model)
@@ -28,31 +30,30 @@ extract_estimates <- function(model,
         est <- est %>%
                dplyr::mutate(estimate = rounding(estimate, fmt),
                              statistic = rounding(statistic, fmt),
-                             statistic = paste0('(', statistic, ')')) 
+                             statistic = paste0('(', statistic, ')'))
     }
+
     # stars
     if (!is.null(stars)) {
         if (!'p.value' %in% colnames(est)) {
             stop('To use the `stars` argument, the `broom::tidy` function must produce a column called "p.value"')
         }
-        if (is.logical(stars)) {
-            if (stars) {
-                stars <- c('*' = .1, '**' = .05, '***' = .01)
-            } 
-        }
-        stars <- sort(stars, decreasing = TRUE)
         est$stars <- ''
         for (n in names(stars)) {
             est$stars <- ifelse(est$p.value < stars[n], n, est$stars)
         }
         est$estimate <- paste0(est$estimate, est$stars)
     }
+
     # prune columns
     est <- est %>% dplyr::select(term, estimate, statistic)
+
     # reshape
     est <- est %>%
            tidyr::gather(statistic, value, -term) %>%
            dplyr::arrange(term, statistic)
+
+
     # output
     return(est)
 }
