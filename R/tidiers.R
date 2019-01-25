f3a4b9d7224e9bcc74500ee454a78495a60752ec
Vincent Arel-Bundock <varel@umich.edu>
2019-01-25 15:22:58 -0500

    tidiers for mice multiple imputation pooled object

---
+++ R/tidiers.R
@@ -0,0 +1,68 @@
+#' @importFrom generics tidy
+#' @export
+generics::tidy
+
+#' @importFrom generics glance
+#' @export
+generics::glance
+
+#' Tidy a multiple imputation `mice` pooled object
+#'
+#' @param x An object returned by one of the `mice::pool` function.
+#' @param ... extra arguments (not used)
+#'
+#' @note
+#' Available stats in mipo object:
+#' \itemize{
+#'      \item estimate
+#'      \item ubar
+#'      \item b
+#'      \item t
+#'      \item dfcom
+#'      \item df
+#'      \item riv
+#'      \item lambda
+#'      \item fmi
+#' }
+#'
+#' @examples
+#' library(mice)
+#' data <- airquality
+#' data[4:10,3] <- rep(NA,7)
+#' data[1:5,4] <- NA
+#' tmp <- mice(data,m=5, seed=500, printFlag = FALSE)
+#' mod <- with(tmp, lm(Temp~ Ozone+Solar.R+Wind))
+#' tidy(mod)
+#'
+#' @export
+#' @family tidiers
+tidy.mira <- function(x, ...) {
+	out <- mice::pool(x, ...) %>%
+           .$pooled %>%
+           dplyr::mutate(term = row.names(.)) %>%
+           tibble::as_tibble() %>%
+           dplyr::select(term, order(names(.)))
+    return(out)
+}
+
+#' Glance a multiple imputation `mice` pooled object
+#'
+#' @param x An object returned by one of the `mice::pool` function.
+#' @param ... extra arguments (not used)
+#'
+#' @examples
+#' library(mice)
+#' data <- airquality
+#' data[4:10,3] <- rep(NA,7)
+#' data[1:5,4] <- NA
+#' tmp <- mice(data,m=5, seed=500, printFlag = FALSE)
+#' mod <- with(tmp, lm(Ozone ~ Solar.R + Wind))
+#' glance(mod)
+#'
+#' @export
+#' @family tidiers
+glance.mira <- function(x, ...) {
+    out <- tibble::tibble('m' = length(x$analyses))
+	out$n <- tryCatch(stats::nobs(x$analyses[[1]]), error = function(e) NULL)
+	return(out)
+}
