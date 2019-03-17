4a3d0741e1d62ff1e4b855f3709b71f5c1d204b0
Vincent Arel-Bundock <varel@umich.edu>
2019-03-17 07:48:23 -0400

    combine rows with same coef_map names

---
+++ tests/testthat/test-bad-input.R
@@ -0,0 +1,28 @@
+context("Test bad input")
+
+library(gt)
+library(MASS)
+library(dplyr)
+library(sandwich)
+library(gtsummary)
+
+test_that("coef_map: two variables with the same name", {
+
+    set.seed(1)
+
+    # within one model produces an error
+    x <- rnorm(100)
+    y <- rnorm(100)
+    z <- rnorm(100)
+    mod <- lm(y ~ x + z)
+    expect_error(gtsummary(mod, coef_map = c('x' = 'X', 'z' = 'X')))
+
+    # across models works well
+    mod <- list()
+    mod[[1]] <- lm(y ~ x)
+    mod[[2]] <- lm(y ~ z)
+    tab <- gtsummary(mod, coef_map = c('x' = 'X', 'z' = 'X'))
+    checkmate::expect_class(tab, 'gt_tbl')
+
+})
+
