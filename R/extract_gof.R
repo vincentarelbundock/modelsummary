1a75d2260d06df5be362275650733a117a13f4a6
Vincent Arel-Bundock <varel@umich.edu>
2020-02-13 16:12:26 -0500

    fix extract_gof

--- R/extract_gof.R
+++ R/extract_gof.R
@@ -5,29 +5,36 @@
 #' @return tibble with goodness-of-fit  statistics
 extract_gof <- function(model, fmt = '%.3f', gof_map = NULL) {
 
+    # define gof_map
+    if (is.null(gof_map)) {
+        gof_map <- modelsummary:::gof_map 
+    }
+
     # extract gof from model object
     gof <- generics::glance(model)
 
-    # extract nobs if not available from glance
+    # extract nobs if not in glance but gof_maps says we want it
     # TODO: This should be fixed upstream in broom
-    if (!'nobs' %in% names(gof)) {
+    if ((!'nobs' %in% names(gof)) & ('nobs' %in% gof_map$raw)) { 
         gof$nobs <- tryCatch(stats::nobs(model, use.fallback = TRUE), error = function(e) NULL)
     }
 
+    # subset and re-order gof
+    gof_map <- gof_map[!gof_map$omit,]
+    idx <- base::intersect(gof_map$raw, colnames(gof))
+    gof_map <- gof_map[gof_map$raw %in% idx,]
+    gof <- gof[gof_map$raw]
+
     # round integer/numeric values
-    fmt_gof <- gof_map$fmt[match(names(gof), gof_map$raw)]
-    fmt_gof[is.na(fmt_gof)] <- fmt
     for (i in seq_along(gof)) {
-        if (class(gof[[i]]) %in% c('numeric', 'integer')) {
-            gof[[i]] <- rounding(gof[[i]], fmt_gof[i])
-        } else {
-            gof[[i]] <- as.character(gof[[i]])
+        if (inherits(gof[[i]], 'numeric')) {
+            gof[[i]] <- rounding(gof[[i]], gof_map$fmt[i])
         }
     }
 
     # reshape
     gof <- gof %>%
-           tidyr::gather(term, value)
+           tidyr::pivot_longer(cols = 1:ncol(.), names_to = 'term')
 
     # output
     return(gof)
