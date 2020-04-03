b33c303e3002002cc9b69550c3298cf034c00378
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-04-03 11:58:38 +0100

    Expand mice tidiers for lm (and fix missing term names)

--- R/tidiers.R
+++ R/tidiers.R
@@ -6,10 +6,10 @@ generics::tidy
 #' @export
 generics::glance
 
-#' Tidy a multiple imputation `mice` pooled object
+#' Tidy a multiple imputation `mice` object
 #'
-#' @param x An object returned by one of the `mice::pool` function.
-#' @param ... extra arguments (not used)
+#' @param x An object with multiply-imputed models from `mice` (class: `mira`)
+#' @param ... extra arguments (not used, unless confidence intervals are requested for multiple `lm` models)
 #'
 #' @note
 #' Available stats in mipo object:
@@ -25,6 +25,13 @@ generics::glance
 #'      \item fmi
 #' }
 #'
+#' @note
+#' If `mira` object consists of `lm`-models, additional results can be returned to facilitate side-by-side tables with other `lm`-model:
+#' \itemize{
+#'      \item p.value
+#'      \item conf.low (if called with conf.int = TRUE)
+#'      \item conf.high (if called with conf.int = TRUE)
+#' }
 #' @examples
 #' library(mice)
 #' data <- airquality
@@ -38,19 +45,63 @@ generics::glance
 #' @export
 #' @family tidiers
 tidy.mira <- function(x, ...) {
+    if (class(x$analyses[[1]]) == "lm") {
+        out <- tidy.mira.lm(x, ...)
+        return(out)
+    } else {
 	out <- mice::pool(x, ...) %>%
            .$pooled %>%
-           dplyr::mutate(term = row.names(.)) %>%
+	       dplyr::mutate(term = as.character(term)) %>%
            tibble::as_tibble() %>%
            dplyr::select(term, order(names(.)))
     return(out)
+    }
+}
+
+#' Tidy  multiple imputations of `lm` models created with `mice`
+#'
+#' @param x A `mira` object containing multiple lm-models based on `mice` imputations.
+#' @param conf.int Logical. Should confidence intervals be returned. Defaults to true.
+#' @param conf.level Confidence level for intervals. Defaults to .95
+#' @param ... extra arguments (not used)
+#'
+#' @note
+#' Available stats in result:
+#' \itemize{
+#'      \item estimate
+#'      \item ubar
+#'      \item b
+#'      \item t
+#'      \item dfcom
+#'      \item df
+#'      \item riv
+#'      \item lambda
+#'      \item fmi
+#'      \item p.value
+#'      \item conf.low (if called with conf.int = TRUE)
+#'      \item conf.high (if called with conf.int = TRUE)
+#' }
+
+
+tidy.mira.lm <- function(x, conf.int = TRUE, conf.level = .95, ...) {
+         out <- summary(mice::pool(x, ...), type = "all", conf.int = conf.int, conf.level = conf.level) %>%
+             dplyr::mutate(term = as.character(term)) %>%
+             tibble::as_tibble()
+
+         conf_vars <- names(out)[stringr::str_detect(names(out), "%")]
+         names(out)[names(out) %in% conf_vars] <- c("conf.low", "conf.high")
+
+         out <- out %>% dplyr::select(term, order(names(.)))
+         return(out)
 }
 
 #' Glance a multiple imputation `mice` pooled object
 #'
-#' @param x An object returned by one of the `mice::pool` function.
+#' @param x An object with multiply-imputed models from `mice` (class: `mira`)
 #' @param ... extra arguments (not used)
 #' @return a tibble with one row
+#'
+#' @note If x contains `lm` models, R2 is included in the output
 #'
 #' @examples
 #' library(mice)
@@ -66,5 +117,9 @@ tidy.mira <- function(x, ...) {
 glance.mira <- function(x, ...) {
     out <- tibble::tibble('m' = length(x$analyses))
 	out$n <- tryCatch(stats::nobs(x$analyses[[1]]), error = function(e) NULL)
+	if (class(x$analyses[[1]]) == "lm") {
+	    out$r.squared <- mice::pool.r.squared(x, adjusted = FALSE)[1]
+	    out$adj.r.squared <- mice::pool.r.squared(x, adjusted = TRUE)[1]
+	}
 	return(out)
 }
