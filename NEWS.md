e96426cd689258fdef89db42cfbdeb98cfa0f21a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-04-08 20:00:04 -0400

    new example and preparing for new CRAN release

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,9 @@
-# 0.2.1: 2020-??-??
+# 0.2.1: 2020-04-04
 
 * Convenience function to render markdown in row/column labels
 * bug: breakage when all GOF were omitted
+* Clean up manual with @keywords internal
+* bug: tidyr import
 
 # 0.2.0: 2020-04-04
 
--- R/gof_map.R
+++ R/gof_map.R
@@ -1,10 +1,21 @@
 #' Data.frame used to clean up and format goodness-of-fit statistics
 #'
+#' By default, this data frame is passed to the 'gof_map' argument of the
+#' 'msummary' or 'modelsummary' functions. Users can modify this data frame to
+#' customize the list of statistics to display and their format. See example
+#' below.
 #' @docType data
 #' @keywords datasets
 #' @name gof_map
 #' @format data.frame with 4 columns of character data: raw, clean, fmt, omit
 #' @importFrom dplyr tribble
+#' @examples
+#' library(modelsummary)
+#' mod <- lm(wt ~ drat, data = mtcars)
+#' gm <- modelsummary::gof_map
+#' gm$omit[gm$raw == 'deviance'] <- FALSE
+#' gm$fmt[gm$raw == 'r.squared'] <- "%.5f"
+#' msummary(mod, gof_map = gm)
 #' @export
 gof_map <- dplyr::tribble(
 	~ raw,           ~ clean,         ~ fmt,     ~ omit,
