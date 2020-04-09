011b470f4de4a7a23625ab8e461133d090bf6a63
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-04-09 09:28:07 -0400

    still not done with omit gof bug

---
+++ tests/testthat/test-gof_map.R
@@ -0,0 +1,23 @@
+context('gof_map')
+
+library(modelsummary)
+
+mod <- list()
+mod$OLS <- lm(am ~ drat, data = mtcars)
+mod$Logit <- glm(am ~ qsec, data = mtcars, family = binomial())
+
+test_that('omit all gof', {
+
+    gof_map_custom <- modelsummary::gof_map
+    gof_map_custom$omit <- TRUE
+    raw <- modelsummary::extract(mod, gof_map = gof_map_custom)
+
+    truth <- c("-1.986", "(0.434)", "0.665", "(0.120)", "", "")
+    expect_equal(truth, unname(raw[[4]]))
+
+    truth <- c("4.739", "(4.045)", "", "", "-0.288", "(0.228)")  
+    expect_equal(truth, unname(raw[[5]]))
+
+})
+
+
