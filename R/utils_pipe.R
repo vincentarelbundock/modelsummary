68f9148354c0d5d6a260db1a30b60bf7bbd085d9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-06-11 08:07:49 -0400

    CRAN bounce %>% reexport

--- R/utils_pipe.R
+++ R/utils_pipe.R
@@ -1,14 +1,3 @@
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#' @return output from rhs(lhs)
-#' @export
 #' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-NULL
+#' @export
+magrittr::`%>%`
