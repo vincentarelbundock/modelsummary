3901fad40d61baa3cf29d792e159e7c250d21768
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-06-30 08:11:52 -0400

    modelplot(background)

--- R/modelplot.R
+++ R/modelplot.R
@@ -6,7 +6,51 @@
 #' @param facet TRUE or FALSE. When the 'models' argument includes several
 #' model objects, TRUE draws terms in separate facets, and FALSE draws terms
 #' side-by-side (dodged).
+#' @param background A list of 'ggplot2' geoms to add to the background of the
+#' plot.  This is especially useful to display annotations "behind" the
+#' 'geom_pointrange' that 'modelplot' draws.
 #' @importFrom ggplot2 ggplot theme_minimal theme element_blank geom_pointrange geom_point aes facet_grid position_dodge labs
+#' @examples
+#' library(modelsummary)
+#' 
+#' # single model
+#' mod <- lm(hp ~ vs + drat, mtcars)
+#' modelplot(mod)
+#' 
+#' # omit terms with string matches or regexes
+#' modelplot(mod, coef_omit = 'Interc')
+#' 
+#' # rename, reorder and subset with 'coef_map'
+#' cm <- c('vs' = 'V-shape engine',
+#'         'drat' = 'Rear axle ratio')
+#' modelplot(mod, coef_map = cm)
+#' 
+#' # several models
+#' models <- list() 
+#' models[['Small model']] <- lm(hp ~ vs, mtcars)
+#' models[['Medium model']] <- lm(hp ~ vs + factor(cyl) , mtcars)
+#' models[['Large model']] <- lm(hp ~ vs + drat + factor(cyl), mtcars)
+#' modelplot(models)
+#' 
+#' # customize your plots with 'ggplot2' functions
+#' library(ggplot2)
+#' 
+#' modelplot(models) +
+#'     scale_color_brewer(type = 'qual') +
+#'     theme_classic()
+#' 
+#' # pass arguments to 'geom_pointrange' through the ... ellipsis
+#' modelplot(mod, color = 'red', size = 1, fatten = .5)
+#' 
+#' # add geoms to the background, behind geom_pointrange
+#' b <- list(geom_vline(xintercept = 0, color = 'orange'),
+#'           annotate("rect", alpha = .1,
+#'                    xmin = -.5, xmax = .5,
+#'                    ymin = -Inf, ymax = Inf),
+#'           geom_point(aes(y = term, x = estimate), alpha = .3,
+#'                      size = 10, color = 'red', shape = 'square'))
+#' modelplot(mod, background = b)
+#' 
 #' @export
 modelplot <- function(models, 
                       conf_level = .95, 
@@ -14,6 +58,7 @@ modelplot <- function(models,
                       coef_omit = NULL, 
                       facet = FALSE,
                       draw = TRUE,
+                      background = NULL,
                       ...) {
 
     modelplot_extract <- function(models, 
@@ -66,6 +111,15 @@ modelplot <- function(models,
     p <- ggplot(dat) +
          theme_minimal() +
          theme(legend.title = element_blank())
+
+    # background geoms
+    if (is.list(background)) {
+        for (g in background) {
+            if (inherits(g, 'ggproto')) {
+                p <- p + g
+            }
+        }
+    }
 
     # geom_pointrange: with confidence interval
     if (!is.null(conf_level)) {
