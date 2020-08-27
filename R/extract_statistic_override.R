5749001e4e7df7fecdb6c75ab58935e4a41fc3b6
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-08-27 09:22:43 -0400

    refactor statistic_override

--- R/extract_statistic_override.R
+++ R/extract_statistic_override.R
@@ -1,97 +1,118 @@
-#' Allow users to override uncertainty estimates
-#' @importFrom broom tidy
+#' Internal method generic to extract statistic override
+#'
+#' @export
+#' @keywords internal
+override_statistic <- function(x, ...) {
+  UseMethod("override_statistic")
+}
+
+
+#' Use the statistic_override vector to extract std.error/p.value/statistic
 #' @param model object type with an available `tidy` method.
 #' @inheritParams modelsummary
+#' @return tibbl
 #' @keywords internal
-#' @return a numeric vector of test statistics
-extract_statistic_override <- function(model, statistic_override, statistic = 'std.error') {
-    out <- NULL
-
-    # lmtest is installed
-    lmtest_installed <- try(base::find.package('lmtest'), silent = TRUE)
-    lmtest_installed <- !'try-error' %in% class(lmtest_installed)
-
-    if (lmtest_installed & (is.matrix(statistic_override) | is.function(statistic_override))) {
-       out <- try(statistic_override_lmtest(model, statistic_override), silent = TRUE)
-    } 
-
-    # lmtest is not installed or errored
-    if (!lmtest_installed | ('try-error' %in% class(out)) | is.null(out)) {
-
-        # function
-        if (is.function(statistic_override)) {
-            out <- try(statistic_override_function(model, statistic_override), silent = TRUE)
-            if ('try-error' %in% class(out)) {
-                stop('The function supplied to `statistic_override` cannot recover a variance-covariance matrix or standard error vector')
-            }
-
-        # matrix
-        } else if (is.matrix(statistic_override)) {
-            out <- try(statistic_override_matrix(model, statistic_override), silent = TRUE)
-            if ('try-error' %in% class(out)) {
-                stop('The matrix supplied to `statistic_override` cannot be used to recover standard errors.')
-            }
-
-        # vector
-        } else if (is.vector(statistic_override)) {
-            out <- try(statistic_override_vector(model, statistic_override, statistic = statistic), silent = TRUE)
-        }
+override_statistic.numeric <- function(x, statistic, ...) {
+  # vector
+  if (is.vector(x)) {
+    out <- tibble::tibble(term = names(x), statistic = x)
+  # matrix
+  } else {
+    out <- try(lmtest::coeftest(model, vcov=x), silent=TRUE)
+    if (!inherits(out, "try-error")) {
+      out <- generics::tidy(out)[, c("term", statistic)]
+    } else {
+      out <- x %>%
+             base::diag() %>%
+             sqrt %>%
+             tibble::tibble(term = names(.), statistic = .)
     }
-
-    return(out)
+  }
+  return(out)
 }
 
-#' Use the lmtest::coeftest function to extract uncertainty estimates
+
+#' Use the statistic_override vector to extract std.error/p.value/statistic
 #' @param model object type with an available `tidy` method.
 #' @inheritParams modelsummary
-#' @return tibble
+#' @return tibbl
 #' @keywords internal
-statistic_override_lmtest <- function(model, statistic_override) {
-    out <- lmtest::coeftest(model, statistic_override) %>%
-           generics::tidy()
-    return(out)
+override_statistic.matrix <- override_statistic.numeric
+
+
+#' Use the statistic_override vector to extract std.error/p.value/statistic
+#' @param model object type with an available `tidy` method.
+#' @inheritParams modelsummary
+#' @return tibbl
+#' @keywords internal
+override_statistic.character <- function(x, ...) {
+  checkmate::assert_atomic_vector(x)
+  out <- tibble::tibble(term = names(x), statistic = x)
+  return(out)
 }
+
 
 #' Use the statistic_override function to extract std.error
 #' @param model object type with an available `tidy` method.
 #' @inheritParams modelsummary
 #' @return tibble
 #' @keywords internal
-statistic_override_function <- function(model, statistic_override) {
-    out <- statistic_override(model) %>%
-           base::diag() %>%
-           tibble::tibble('term' = names(.),
-                          'std.error' = .)
-    return(out)
+override_statistic.function <- function(x, model, statistic, ...) {
+
+  out <- try(lmtest::coeftest(model, vcov=x), silent=TRUE)
+
+  if (inherits(out, "coeftest")) {
+    out <- generics::tidy(out)[, c("term", statistic)]
+  }
+
+  if (inherits(out, "try-error")) {
+    out <- try(x(model), silent=TRUE)
+  }
+
+  if (inherits(out, "matrix") || inherits(out, "numeric")) {
+    out <- override_statistic.numeric(out, model=model, statistic=statistic) 
+  }
+
+  if (inherits(out, "character")) {
+    out <- override_statistic.character(out, model=model, statistic=statistic)
+  }
+
+  if (inherits(out, "try-error")) {
+    stop("Could not retrieve a valid variance-covariance matrix using the
+         function supplied in `statistic_override`.")
+  }
+
+  return(out)
 }
 
-#' Use the statistic_override matrix to extract std.error
-#' @param model object type with an available `tidy` method.
-#' @inheritParams modelsummary
-#' @return tibble
-#' @keywords internal
-statistic_override_matrix <- function(model, statistic_override) {
-    if (is.null(names(statistic_override))) {
-        stop('The colnames and row.names of the `statistic_override` matrix must correspond to term/coefficient names.')
-    }
-    out <- statistic_override %>%
-           base::diag() %>%
-           tibble::tibble('term' = names(.),
-                          'std.error' = .)
-    return(out)
-}
 
-#' Use the statistic_override vector to extract std.error/p.value/statistic
+#' Allow users to override uncertainty estimates
+#' @importFrom broom tidy
 #' @param model object type with an available `tidy` method.
 #' @inheritParams modelsummary
-#' @return tibble
 #' @keywords internal
-statistic_override_vector <- function(model, statistic_override, statistic) {
-    if (is.null(names(statistic_override))) {
-        stop('The names of the `statistic_override` vector must correspond to term/coefficient names.')
-    }
-    out <- statistic_override %>%
-           tibble::tibble(term = names(.), statistic = .) %>%
-           stats::setNames(c('term', statistic))
-    return(out)
+#' @return a numeric vector of test statistics
+extract_statistic_override <- function(model, statistic_override,
+                                       statistic="std.error") {
+
+  checkmate::assert(
+    checkmate::check_function(statistic_override),
+    checkmate::check_matrix(statistic_override),
+    checkmate::check_atomic_vector(statistic_override),
+    combine="or")
+  
+  if (is.vector(statistic_override)) {
+    checkmate::assert_true(all(names(statistic_override) == names(statistic_override)))
+  }
+
+  if (is.matrix(statistic_override)) {
+    checkmate::assert_true(all(colnames(statistic_override) == names(statistic_override)))
+  }
+
+  out <- override_statistic(x=statistic_override, model=model,
+                            statistic=statistic)
+  colnames(out)[2] <- statistic
+
+  return(out)
+
 }
