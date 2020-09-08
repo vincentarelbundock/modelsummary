f152e5083e6b7e4853f168518c8b6f5001885c9d
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-09-08 14:43:22 -0400

    test glance_custom

--- tests/testthat/test-glance_custom.R
+++ tests/testthat/test-glance_custom.R
@@ -2,15 +2,20 @@ context('glance_custom')
 
 library(modelsummary)
 
-# # for some reason the new method won't get picked up inside
-# test_that("fixed value glance_custom.glm", {
-#   glance_custom.glm <- function(x) {
-#     data.frame("test"=1.54, "test2"="lkkd", "test3"=as.integer(2),
-#                "test4"=TRUE)
-#   }
-#   mod <- glm(am ~ mpg, mtcars, family = binomial)
-#   out <- msummary(mod, "data.frame")
-#   expect_equal(dim(out), c(12, 4))
-#   rm("glance_custom.glm")
-# })
+test_that("fixed value glance_custom.glm", {
+  glance_custom.glm <- function(x) {
+    data.frame("test"=1.54, "test2"="lkkd", "test3"=as.integer(2),
+               "test4"=TRUE)
+  }
+
+  # hack to fix testthat scoping issue
+  assign("glance_custom.glm", glance_custom.glm, envir=.GlobalEnv)
+
+  mod <- glm(am ~ mpg, mtcars, family = binomial)
+  out <- msummary(mod, "data.frame")
+  expect_equal(dim(out), c(12, 4))
+
+  # hack to fix testthat scoping issue
+  rm("glance_custom.glm", envir=.GlobalEnv)
+})
 
