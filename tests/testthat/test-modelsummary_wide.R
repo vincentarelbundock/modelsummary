25a3519977cb79283a56f7a96872536fd84c5f8f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-12-08 12:55:29 -0500

    test uncomment modelsummary_wide

--- tests/testthat/test-modelsummary_wide.R
+++ tests/testthat/test-modelsummary_wide.R
@@ -1,45 +1,46 @@
-# context("modelsummary_wide")
+context("modelsummary_wide")
 
-# library(nnet)
+library(nnet)
+library(modelsummary)
 
-# test_that("no group", {
-#   mod <- list(
-#     lm(hp ~ mpg, mtcars),
-#     lm(hp ~ mpg + drat + vs, mtcars)
-#   )
-#   expect_error(modelsummary_wide(mod))
-# })
-
-
-# test_that("nnet::multinom one model", {
-#   var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
-#   var2 <- sample(c(0,1), size=100, replace=T)
-#   var3 <- rnorm(100, mean=10, sd=2)
-#   df <- data.frame(var1, var2, var3)
-#   invisible(capture.output(mod <- nnet::multinom(var1~var2, data=df)))
-#   tmp <- modelsummary_wide(mod, output="data.frame")
-#   expect_is(tmp, "data.frame")
-#   expect_equal(dim(tmp), c(7, 5))
-# })
+test_that("no group", {
+  mod <- list(
+    lm(hp ~ mpg, mtcars),
+    lm(hp ~ mpg + drat + vs, mtcars)
+  )
+  expect_error(modelsummary_wide(mod))
+})
 
 
-# test_that("nnet::multinom two models", {
+test_that("nnet::multinom one model", {
+  var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
+  var2 <- sample(c(0,1), size=100, replace=T)
+  var3 <- rnorm(100, mean=10, sd=2)
+  df <- data.frame(var1, var2, var3)
+  invisible(capture.output(mod <- nnet::multinom(var1~var2, data=df)))
+  tmp <- modelsummary_wide(mod, output="data.frame")
+  expect_is(tmp, "data.frame")
+  expect_equal(dim(tmp), c(7, 5))
+})
 
-#   var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
-#   var2 <- sample(c(0,1), size=100, replace=T)
-#   var3 <- rnorm(100, mean=10, sd=2)
-#   df1 <- data.frame(var1, var2, var3)
 
-#   var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
-#   var2 <- sample(c(0,1), size=100, replace=T)
-#   var3 <- rnorm(100, mean=10, sd=2)
-#   df2 <- data.frame(var1, var2, var3)
+test_that("nnet::multinom two models", {
 
-#   invisible(capture.output(m1 <- nnet::multinom(var1~var2, data=df1)))
-#   invisible(capture.output(m2 <- nnet::multinom(var1~var2, data=df2)))
+  var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
+  var2 <- sample(c(0,1), size=100, replace=T)
+  var3 <- rnorm(100, mean=10, sd=2)
+  df1 <- data.frame(var1, var2, var3)
 
-#   tmp <- modelsummary_wide(list(m1, m2), output="data.frame")
-#   expect_is(tmp, "data.frame")
-#   expect_equal(dim(tmp), c(16, 5))
+  var1 <- sample(c('A', 'B', 'C'), replace = T, size=100)
+  var2 <- sample(c(0,1), size=100, replace=T)
+  var3 <- rnorm(100, mean=10, sd=2)
+  df2 <- data.frame(var1, var2, var3)
 
-# })
+  invisible(capture.output(m1 <- nnet::multinom(var1~var2, data=df1)))
+  invisible(capture.output(m2 <- nnet::multinom(var1~var2, data=df2)))
+
+  tmp <- modelsummary_wide(list(m1, m2), output="data.frame")
+  expect_is(tmp, "data.frame")
+  expect_equal(dim(tmp), c(16, 5))
+
+})
