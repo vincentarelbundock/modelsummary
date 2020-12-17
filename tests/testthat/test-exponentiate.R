e83912b0ce6619371ea8679324d4bc5ef299fddd
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-12-17 10:29:57 -0500

    exponentiate warning

---
+++ tests/testthat/test-exponentiate.R
@@ -0,0 +1,16 @@
+context("exponentiate coefficients") 
+
+library(modelsummary)
+
+fit = glm(am  ~ mpg + factor(cyl), 
+          data = mtcars , 
+          family = binomial ) 
+
+test_that("logit coefficients exponentiate", {
+  tab = modelsummary(fit, gof_omit=".*", statistic=NULL, output="dataframe",
+                     exponentiate=TRUE)
+  tab = unname(as.numeric(tab[[4]]))
+  truth = unname(round(exp(coef(fit)), 3))
+  expect_equal(truth, tab)
+})
+
