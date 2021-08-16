d2cb6d9d69beedd0d9e32daa4419b518007d6d29
Grant McDermott <grant.mcdermott@gmail.com>
2021-08-16 14:32:28 -0700

    Add glance custom method for did package objects (MP and AGGTEobj)

--- R/glance_custom.R
+++ R/glance_custom.R
@@ -78,6 +78,27 @@ glance_custom_internal.felm <- function(x, vcov_type = NULL, ...) {
     return(out)
 }
 
+#' @inherit glance_custom_internal
+#' @keywords internal
+glance_custom_internal.MP <- function(x, vcov_type = NULL, ...) {
+  cli::assert_dependency("did")
+  out <- data.frame(row.names = "firstrow")
+  if (is.null(vcov_type) || !vcov_type %in% c("vector", "matrix", "function")) {
+    if (x$DIDparams$bstrap) {
+      if (!is.null(x$DIDparams$clustervars)) {
+        cluster_vars = paste(x$DIDparams$clustervars, collapse = " & ")
+      } else {
+        cluster_vars = x$DIDparams$idname
+      }
+      out[['vcov.type']] <- paste0("Clustered (", cluster_vars, ")")
+    }
+  }
+  row.names(out) <- NULL
+  return(out)
+}
+#' @inherit glance_custom_internal
+#' @keywords internal
+glance_custom_internal.AGGTEobj <- glance_custom_internal.MP
 
 ##' @inherit glance_custom
 ##' @noRd
