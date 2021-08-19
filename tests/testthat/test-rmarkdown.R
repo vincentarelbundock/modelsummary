2e16899ce8f450c0f9e0708f125dd13bba834b9a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2021-08-19 19:54:59 -0400

    tests: fix rmarkdown to word

--- tests/testthat/test-rmarkdown.R
+++ tests/testthat/test-rmarkdown.R
@@ -7,6 +7,7 @@ skip_on_cran()
 dangerous_document <- '
 ---
 title: test
+output: %s
 ---
 
 Table \\ref{tab:simple}
@@ -41,40 +42,38 @@ if (knitr::is_latex_output()) {
 ```
 '
 rmd_file <- tempfile(fileext = ".Rmd")
-cat(dangerous_document, file = rmd_file)
 
 test_that("Rmarkdown to pdf_document", {
+    cat(sprintf(dangerous_document, "pdf_document"), file = rmd_file)
     pdf_file <- gsub("\\.Rmd$", ".pdf", rmd_file )
     expect_error(rmarkdown::render(rmd_file,
-                                   output_format = "pdf_document",
                                    output_file = pdf_file,
                                    quiet = TRUE),
                  NA)
 })
 
 test_that("Rmarkdown to html_document", {
+    cat(sprintf(dangerous_document, "html_document"), file = rmd_file)
     html_file <- gsub("\\.Rmd$", ".html", rmd_file )
     expect_error(rmarkdown::render(rmd_file,
-                                   output_format = "html_document",
                                    output_file = html_file,
                                    quiet = TRUE),
                  NA)
 })
 
 test_that("Rmarkdown to word_document", {
+    cat(sprintf(dangerous_document, "word_document"), file = rmd_file)
     docx_file <- gsub("\\.Rmd$", ".docx", rmd_file )
     expect_error(rmarkdown::render(rmd_file,
-                                   output_format = "word_document",
                                    output_file = docx_file,
                                    quiet = TRUE),
                  NA)
 })
 
-
 test_that("Rmarkdown to bookdown::word_document2", {
+    cat(sprintf(dangerous_document, "bookdown::word_document2"), file = rmd_file)
     docx_file <- gsub("\\.Rmd$", ".docx", rmd_file )
-    expect_error(rmarkdown::render(rmd_filej,
-                                   output_format = "bookdown::word_document2",
+    expect_error(rmarkdown::render(rmd_file,
                                    output_file = docx_file,
                                    quiet = TRUE),
                  NA)
