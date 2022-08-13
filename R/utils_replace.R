a560f53d35183f2a6628039a736afe3e3ef4fbf4
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-08-13 09:11:39 -0400

    bugfix labels

--- R/utils_replace.R
+++ R/utils_replace.R
@@ -1,16 +1,15 @@
 #' replace character vector entries using a named vector dictionary
 #' @noRd
 #' 
-replace_dict <- function(x, dict, interaction = TRUE) {
-  # this is simpler and faster, but does not handle interactions
-  # for (i in seq_along(dict)) {
-  #   x[x == names(dict)[i]] <- dict[i]
-  # }
+replace_dict <- function(x, dict, interaction = FALSE) {
   for (i in seq_along(dict)) {
-    x <- gsub(sprintf("^%s$", names(dict)[i]), dict[i], x)
-    x <- gsub(sprintf(":%s$", names(dict)[i]), paste0(":", dict[i]), x)
-    x <- gsub(sprintf("^%s:", names(dict)[i]), paste0(dict[i], ":"), x)
-    x <- gsub(sprintf(":%s:", names(dict)[i]), paste0(":", dict[i], ":"), x)
+    x[x == names(dict)[i]] <- dict[i]
+    if (interaction) {
+      # can't use regex because of "(Intercept)" and similar user-defined arbitrary strings
+      x <- gsub(sprintf(":%s:", names(dict)[i]), paste0(":", dict[i], ":"), x, fixed = TRUE)
+      x <- gsub(sprintf(":%s", names(dict)[i]), paste0(":", dict[i]), x, fixed = TRUE)
+      x <- gsub(sprintf("%s:", names(dict)[i]), paste0(dict[i], ":"), x, fixed = TRUE)
+    }
   }
 
   return(x)
