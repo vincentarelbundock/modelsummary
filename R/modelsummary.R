6b88ab460643c8b7948a53532dc3350d94713f47
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-12-23 19:50:08 -0500

    docs

--- R/modelsummary.R
+++ R/modelsummary.R
@@ -1,6 +1,6 @@
 # https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when#comment20826625_12429344
-# 2012 hadley says "globalVariables is a hideous hack and I will never use it"
-# 2014 hadley updates his own answer with globalVariables as one of "two solutions"
+# 2012 wickham says "globalVariables is a hideous hack and I will never use it"
+# 2014 wickham updates his own answer with globalVariables as one of "two solutions"
 globalVariables(c('.', 'term', 'part', 'estimate', 'conf.high', 'conf.low',
 'value', 'p.value', 'std.error', 'statistic', 'stars_note', 'logLik',
 'formatBicLL', 'section', 'position', 'where', 'ticks', 'statistic1', 'model',
@@ -32,7 +32,12 @@ globalVariables(c('.', 'term', 'part', 'estimate', 'conf.high', 'conf.low',
 #' 
 #' @template modelsummary_examples
 #'
-#' @param models a model or (optionally named) list of models
+#' @param models a model or a (named) list of models
+#' * Single model: `modelsummary(model)`
+#' * Unnamed list: `modelsummary(list(model1, model2))`
+#'   - Models are labelled automatically. The default label style can be altered by setting a global option. See below.
+#' * Named list: `modelsummary(list("A"=model1, "B"=model2))`
+#'   - Models are labelled using the list names.
 #' @param output filename or object type (character string)
 #' * Supported filename extensions: .docx, .html, .tex, .md, .txt, .png, .jpg.
 #' * Supported object types: "default", "html", "markdown", "latex", "latex_tabular", "data.frame", "gt", "kableExtra", "huxtable", "flextable", "DT", "jupyter". The "modelsummary_list" value produces a lightweight object which can be saved and fed back to the `modelsummary` function.
