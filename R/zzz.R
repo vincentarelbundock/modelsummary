6a4e0d053b22f77d83de095941310a7fb077f707
Vincent Arel-Bundock <vincentarelbundock@users.noreply.github.com>
2023-04-05 20:39:54 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname){
     pkgs <- c("kableExtra", "gt", "flextable", "huxtable", "DT")
     flags <- insight::check_if_installed(pkgs, quietly = TRUE)
-    if (!any(flags) && !isTRUE(config_get("output") == "markdown")) {
+    if (!any(flags) && !isTRUE(config_get("factory_default") == "markdown")) {
         msg <- c(
             '`modelsummary` has built-in support to draw text-only (markdown) tables. To generate tables in other formats, you must install one or more of these libraries:',
             '
