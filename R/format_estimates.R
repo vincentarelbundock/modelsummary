b6f15d91552a06364b6beef80c7be18155f2ee4a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-04-24 16:06:14 -0400

    bug fix: fmt = NULL

--- R/format_estimates.R
+++ R/format_estimates.R
@@ -130,7 +130,11 @@ format_estimates <- function(
   }
 
   # needed to avoid empty glues "p =""
-  est[is.na(est)] <- ""
+  for (i in seq_along(est)) {
+    if (is.character(est[[i]])) {
+      est[[i]][is.na(est[[i]])] <- ""
+    }
+  }
 
   # modelplot safety hack: statistics may not be available for some models (e.g., "brms")
   if (identical(estimate_glue, "{estimate}|{std.error}|{conf.low}|{conf.high}|{p.value}")) {
