1a61741e909c96572a0c1c7ffeb5ed51399f32b9
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-02-26 19:25:45 -0500

    test

--- inst/tinytest/test-rmarkdown.R
+++ inst/tinytest/test-rmarkdown.R
@@ -1,8 +1,9 @@
 # solaris failure and complaints about pandoc in "Writing R Extensions" ("annoyingly so")
 # pkgdown failure on github actions
+exit_file("tinytable: escape latex")
 source("helpers.R")
 if (ON_CI) exit_file("CI")
-exit_file("tinytable + rmarkdown")
+# exit_file("tinytable + rmarkdown")
 
 dangerous_document <- '
 ---
@@ -35,9 +36,9 @@ mod <- lm(hp ~ under_score + drat, dat)
 modelsummary(mod, title = "Some caption content.")
 ```
 
-```{r siunitx}
+```{r siunitx, eval = FALSE}
 if (knitr::is_latex_output()) {
-  modelsummary(mod, title = "Some caption content.", align = "ld")
+  modelsummary(mod, title = "Some caption content.", align = "lc")
 }
 ```
 '
@@ -50,25 +51,25 @@ pdf_file <- gsub("\\.Rmd$", ".pdf", rmd_file )
 rmarkdown::render(rmd_file, output_file = pdf_file, quiet = TRUE)
 
 
-# Rmarkdown to html_document
-cat(sprintf(dangerous_document, "html_document"), file = rmd_file)
-html_file <- gsub("\\.Rmd$", ".html", rmd_file )
-rmarkdown::render(rmd_file, output_file = html_file, quiet = TRUE)
+# # Rmarkdown to html_document
+# cat(sprintf(dangerous_document, "html_document"), file = rmd_file)
+# html_file <- gsub("\\.Rmd$", ".html", rmd_file )
+# rmarkdown::render(rmd_file, output_file = html_file, quiet = TRUE)
 
 
-# Rmarkdown to word_document
-cat(sprintf(dangerous_document, "word_document"), file = rmd_file)
-docx_file <- gsub("\\.Rmd$", ".docx", rmd_file )
-rmarkdown::render(rmd_file, output_file = docx_file, quiet = TRUE)
-
-
-# does not even work interactively
-# # Rmarkdown to bookdown::word_document2
-# cat(sprintf(dangerous_document, "bookdown::word_document2"), file = rmd_file)
+# # Rmarkdown to word_document
+# cat(sprintf(dangerous_document, "word_document"), file = rmd_file)
 # docx_file <- gsub("\\.Rmd$", ".docx", rmd_file )
 # rmarkdown::render(rmd_file, output_file = docx_file, quiet = TRUE)
 
+
+# # does not even work interactively
+# # # Rmarkdown to bookdown::word_document2
+# # cat(sprintf(dangerous_document, "bookdown::word_document2"), file = rmd_file)
+# # docx_file <- gsub("\\.Rmd$", ".docx", rmd_file )
+# # rmarkdown::render(rmd_file, output_file = docx_file, quiet = TRUE)
+
 unlink(rmd_file)
-unlink(pdf_file)
-unlink(docx_file)
-unlink(html_file)
+# unlink(pdf_file)
+# unlink(docx_file)
+# unlink(html_file)
