fae7b99784ce539c1022f7e9baa08f385f83b25a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-03-04 20:09:39 -0500

    output

--- R/sanitize_output.R
+++ R/sanitize_output.R
@@ -189,12 +189,13 @@ sanitize_output <- function(output) {
   # choose factory based on output_format
   output_factory <- factory_dict[[output_format]]
 
-  # appears to be needed otherwise not treated as_is in Quarto
-  if (output_factory == 'tinytable') {
-    if (isTRUE(check_dependency('knitr')) && knitr::is_latex_output()) {
-      output_format <- "latex"
-    }
-  }
+  # this breaks datasummary_skim which needs plot_tt() in two steps
+  # # appears to be needed otherwise not treated as_is in Quarto
+  # if (output_factory == 'tinytable') {
+  #   if (isTRUE(check_dependency('knitr')) && knitr::is_latex_output()) {
+  #     output_format <- "latex"
+  #   }
+  # }
 
   # kableExtra must specify output_format ex ante (but after factory choice)
   if (output_factory == 'kableExtra' && output_format %in% c('default', 'kableExtra')) {
