13b44012220af21f498dbcd35e1f338b3b322c49
florence-laflamme <florence.laflamme@umontreal.ca>
2024-03-28 15:53:49 -0400

    New tests without snapshots

--- inst/tinytest/test-datasummary_correlation.R
+++ inst/tinytest/test-datasummary_correlation.R
@@ -3,37 +3,25 @@ requiet("tinysnapshot")
 using("tinysnapshot")
 if (!requiet("correlation")) exit_file("correlation package")
 
-
 x <- mtcars[, 1:4]
 co <- correlation(x)
 tab <- datasummary_correlation(co, output = "dataframe", stars = TRUE)
 expect_equivalent(tab[[2]], c("1", "-.85***", "-.85***", "-.78***"))
 
 # basics
-expect_snapshot_print(datasummary_correlation(x, output = "latex"), label= "datasummary_correlation-basics")
-
-# easycorrelation
-if (!require(correlation)) {
-  install.packages(correlation, dependencies = TRUE)
-  library(correlation, character.only = TRUE)
-}
-x <- correlation(x)
-expect_snapshot_print(datasummary_correlation(x, output = "latex"), label= "datasummary_correlation-easycorrelation")
+tab <- datasummary_correlation(x, output = "dataframe")
+expect_equivalent(tab[[3]], c(".", "1", ".90", ".83"))
 
 dat <- mtcars[, c("mpg", "hp")]
 colnames(dat) <- c("Miles / Gallon", "Horse Power")
 
 # alternative methods
-expect_snapshot_print(datasummary_correlation(dat, method = "pearspear", output = "latex"), label= "datasummary_correlation-alternative_methods")
+tab <- datasummary_correlation(dat, method = "pearspear", output = "dataframe")
+expect_equivalent(tab[[2]], c("1", "-.89"))
 
 # custom function
 cor_fun <- function(x) cor(x, method = "kendall")
-expect_snapshot_print(datasummary_correlation(dat, method = cor_fun, output = "latex"), label= "datasummary_correlation-custom_function")
-
-# rename columns alphabetically and include a footnote for reference
-note <- sprintf("(%s) %s", letters[1:ncol(dat)], colnames(dat))
-note <- paste(note, collapse = "; ")
-colnames(dat) <- sprintf("(%s)", letters[1:ncol(dat)])
-expect_snapshot_print(datasummary_correlation(dat, notes = note, output = "latex"), label= "datasummary_correlation-alphabetical_columns_and_footnotes")
+table <- datasummary_correlation(dat, method = cor_fun, output = "dataframe")
+expect_equivalent(table[[3]], c("-.74", "1.00"))
 
 
