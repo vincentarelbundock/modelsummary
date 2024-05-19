09665c7880d6747fa747e806b817faa8148bdd0c
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-05-19 08:18:53 -0400

    bug

--- R/methods_stats.R
+++ R/methods_stats.R
@@ -20,6 +20,7 @@ glance_custom_internal.lm <- function(x, vcov_type = NULL, gof = NULL, ...) {
     if (inherits(gof, "data.frame") && "statistic" %in% colnames(gof)) {
       out[["F"]] <- gof$statistic
     } else {
+      fstat <- NULL
       if (isTRUE(check_dependency("lmtest"))) {
         void <- utils::capture.output(fstat <- try(lmtest::waldtest(x, vcov = stats::vcov)$F[2], silent = TRUE))
       }
