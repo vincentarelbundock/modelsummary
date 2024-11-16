fd20f53b16c3e4c8a06441eb3c6b802627960b54
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-16 08:53:46 -0500

    snapshots

--- R/modelsummary_wide.R
+++
@@ -1,8 +0,0 @@
-#' Superseded function
-#'
-#' This function is superseded by the `shape` argument of the `modelsummary` function. 
-#' @keywords internal
-#' @export
-modelsummary_wide <- function(...) {
-  stop("The `modelsummary_wide` is replaced by the `shape` argument of the `modelsummary` function.", call. = FALSE)
-}
