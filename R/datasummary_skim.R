4285a64141f246d20f84d14a4f483985227a74a0
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-20 19:27:03 -0500

    issue #822 document fun_unique in datasummary_skim

--- R/datasummary_skim.R
+++ R/datasummary_skim.R
@@ -11,7 +11,7 @@
 #' @import data.table
 #' @param type String. Variables to summarize: "all", "numeric", "categorical", "dataset"
 #' @param by Character vector of grouping variables to compute statistics over.
-#' @param fun_numeric Named list of funtions to apply to each column of `data`. If `fun_numeric` includes "Histogram" or "Density", inline plots are inserted.
+#' @param fun_numeric Named list of funtions to apply to each numeric column of `data`. If `fun_numeric` includes "Histogram" or "Density", inline plots are inserted. This argument is only used when `type="numeric"` or `"all"`.
 #'
 #' @template kableExtra2tinytable
 #' @template citation
