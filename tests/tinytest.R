476cf43b6e24ebdd368fb2624d945b146604e512
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-08-24 22:10:06 -0400

    runnersdown

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,6 @@
-
-if (isTRUE(Sys.getenv("R_NOT_CRAN") == "true")) {
-  if (requireNamespace("tinytest", quietly = TRUE)) {
-    tinytest::test_package("modelsummary")
-  }
+run <- FALSE
+pkg <- requireNamespace("tinytest", quietly = TRUE)
+home <- grepl("(?i)vince", Sys.info()["nodename"]) || identical(Sys.getenv("R_NOT_CRAN"), "true")
+if (pkg && home && run) {
+  tinytest::test_package("marginaleffects")
 }
