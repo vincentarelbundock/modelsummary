14ad609527aca8e7db9c6cbbcf98b56eaa6adc40
etiennebacher <etienne.bacher@protonmail.com>
2026-04-12 22:14:47 +0200

    init

--- R/escape.R
+++ R/escape.R
@@ -38,8 +38,8 @@ escape_latex <- function(x, newlines = FALSE, spaces = FALSE) {
   x <- gsub("\\\\textbackslash", "\\\\textbackslash{}", x)
   x <- gsub("~", "\\\\textasciitilde{}", x)
   x <- gsub("\\^", "\\\\textasciicircum{}", x)
-  if (newlines) x <- gsub("(?<!\n)\n(?!\n)", "\\\\\\\\", x, perl <- TRUE)
-  if (spaces) x <- gsub("(?<<- ) ", "\\\\ ", x, perl <- TRUE)
+  if (newlines) x <- gsub("(?<!\n)\n(?!\n)", "\\\\\\\\", x, perl = TRUE)
+  if (spaces) x <- gsub("(?<<- ) ", "\\\\ ", x, perl = TRUE)
   x
 }
 
