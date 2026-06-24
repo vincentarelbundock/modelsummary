bbff0bb677e0070fe4b03c193ff86d01d3dce640
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2026-06-23 22:49:11 -0400

    Fix coef_end add_rows separator placement

--- inst/tinytest/test-add_rows.R
+++ inst/tinytest/test-add_rows.R
@@ -24,6 +24,23 @@ tab <- modelsummary(mod, add_rows = rows, output = "data.frame")
 expect_equivalent(ncol(tab), 5)
 expect_true(nrow(tab) > 14)
 
+# Issue #964: "coef_end" rows belong above the coef/gof separator.
+line_style_rows <- function(tab) {
+  idx <- vapply(tab@lazy_style, function(z) identical(z$line, "b"), logical(1))
+  as.integer(vapply(tab@lazy_style[idx], function(z) z$i, numeric(1)))
+}
+
+new_row <- data.frame(term = "test", `(1)` = "new row", check.names = FALSE)
+attr(new_row, "position") <- "coef_end"
+tab <- modelsummary(lm(mpg ~ hp, data = mtcars), add_rows = new_row, output = "tinytable")
+expect_true(match("test", tab@data[[1]]) < match("Num.Obs.", tab@data[[1]]))
+expect_equivalent(line_style_rows(tab), 5)
+
+attr(new_row, "position") <- "gof_start"
+tab <- modelsummary(lm(mpg ~ hp, data = mtcars), add_rows = new_row, output = "tinytable")
+expect_true(match("test", tab@data[[1]]) < match("Num.Obs.", tab@data[[1]]))
+expect_equivalent(line_style_rows(tab), 4)
+
 # add_rows numeric are formatted by fmt
 tmp <- data.frame(a = 1:2, b = 2:3, c = 3:4)
 tab <- datasummary(
@@ -34,5 +51,4 @@ tab <- datasummary(
   output = "dataframe"
 )
 expect_equivalent(tab$sd, c("69", "6", "3", "4"))
-
 
